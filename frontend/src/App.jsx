60168de146c71da94ab5f95ad95b4865b8b553e5
imvizay <vizzaymeena@gmail.com>
2026-03-03 17:20:55 +0530

    Redesign some component to be compaitable for nested component inside them so created brandIntro as the child component of the base.jsx which will play as a base role to be present everywhere across the web page as header and footer , also made a component for otp verification.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 // routing tools
 import { Route,Routes } from 'react-router-dom'
 // components
-import BrandIntro from './components/brand-intro/BrandIntro'
+import BaseLayout from './layout/Base'
 
-import Authentication from './components/forms/Authentication' // authentication form
+import BrandIntro from './components/brand-intro/BrandIntro'
+import RegisterAccount from './components/forms/Register'
 
 
 export default function App() {
@@ -11,8 +12,11 @@ export default function App() {
     <>
     <Routes>
       {/* onboarding introduction component */}
-      <Route path='/' element={<BrandIntro/>}/>
-         
+      <Route path='/' element={<BaseLayout/>}>
+         <Route index element={<BrandIntro/>}/>
+         <Route path = "register" element={<RegisterAccount/>}/>
+      </Route>
+     
 
     </Routes>
     </>
