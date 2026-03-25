e61b39ef4d2199d5eb2c83e411036bc17e7e7f83
vizaymeena <vizzaymeena@gmail.com>
2026-03-25 18:44:05 +0530

    Update prod.py

--- backend/project/config/settings/prod.py
+++ backend/project/config/settings/prod.py
@@ -19,7 +19,6 @@ CHANNEL_LAYERS = {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
             "hosts": [os.getenv("REDIS_URL")],
-            "ssl":True # req for redis cloud
         },
     },
 }
